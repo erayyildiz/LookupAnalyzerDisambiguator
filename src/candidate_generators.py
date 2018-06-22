d4f583f17951bb193795e483de67659f7364ae44
Eray Yıldız <eray.yildiz@alterna-team.com>
2018-06-22 14:16:08 +0300

    First commit

---
+++ src/candidate_generators.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+from utils import to_lower, asciify
+import re
+import math
+import logging.config
+
+logging.config.fileConfig('resources/logging.ini')
+logger = logging.getLogger(__file__)
+
+
+class TurkishStemSuffixCandidateGenerator(object):
+
+    ROOT_TRANSFORMATION_MAP = {"tıp": "tıb", "prof.": "profesör", "dr.": "doktor",
+                               "yi": "ye", "ed": "et", "di": "de"}
+    TAG_FLAG_MAP = {0: "Adj", 1: "Adverb", 2: "Conj", 3: "Det", 4: "Dup", 5: "Interj", 6: "Noun", 7: "Postp",
+                    8: "Pron", 9: "Ques", 10: "Verb", 11: "Num", 12: "Noun+Prop"}
+
+    SUFFIX_DICT_FILE_PATH = "resources/Suffixes&Tags.txt"
+    STEM_LIST_FILE_PATH = "resources/StemListWithFlags.txt"
+
+    CONSONANT_STR = "[bcdfgğhjklmnprsştvyzxwqBCDFGĞHJKLMNPRSŞTVYZXWQ]"
+    VOWEL_STR = "[aeıioöuüAEIİOÖUÜ]"
+    WIDE_VOWELS_STR = "[aeoöAEOÖ]"
+    NARROW_VOWELS_STR = "[uüıiUÜIİ]"
+
+    ENDS_WITH_SOFT_CONSONANTS_REGEX = re.compile(r"^.*[bcdğBCDĞgG]$")
+    SUFFIX_TRANSFORMATION_REGEX1 = re.compile(r"[ae]")
+    SUFFIX_TRANSFORMATION_REGEX2 = re.compile(r"[ıiuü]")
+    ENDS_TWO_CONSONANT_REG = re.compile(r"^.*{}{}$".format(CONSONANT_STR, CONSONANT_STR))
+    STARTS_VOWEL_REGEX = re.compile(r"^{}.*$".format(VOWEL_STR))
+    ENDS_NARROW_REGEX = re.compile(r"^.*{}$".format(NARROW_VOWELS_STR))
+    TAG_SEPARATOR_REGEX = re.compile(r"[\+\^]")
+    NON_WORD_REGEX = re.compile(r"^[^A-Za-zışğüçöÜĞİŞÇÖ]+$")
+
+    def __init__(self, case_sensitive=True, asciification=False, suffix_normalization=False):
+        self.case_sensitive = case_sensitive
+        self.asciification = asciification
+        self.suffix_normalization = suffix_normalization
+        self.suffix_dic = {}
+        logger.info("Reading suffix-tag map...")
+        self.read_suffix_dic()
+        logger.info("Done.")
+        self.stem_dic = {}
+        logger.info("Reading stem list")
+        self.read_stem_list()
+        logger.info("Done.")
+
+    def read_suffix_dic(self):
+        with open(TurkishStemSuffixCandidateGenerator.SUFFIX_DICT_FILE_PATH, "r", encoding="UTF-8") as f:
+            for line in f:
+                splits = line.strip().split("\t")
+                suffix = splits[0]
+                if suffix not in self.suffix_dic:
+                    self.suffix_dic[suffix] = []
+                self.suffix_dic[suffix].append(splits[1])
+                self.stem_dic = []
+
+    def read_stem_list(self):
+        with open(TurkishStemSuffixCandidateGenerator.STEM_LIST_FILE_PATH, "r", encoding="UTF-8") as f:
+            for line in f:
+                splits = line.strip().split("\t")
+                stem = splits[0]
+                if not self.case_sensitive:
+                    stem = to_lower(stem)
+                flag = int(splits[1].strip())
+                postags = TurkishStemSuffixCandidateGenerator._parse_flag(flag)
+                if stem in self.stem_dic:
+                    self.stem_dic[stem] = list(set(list(postags) + self.stem_dic[stem]))
+                else:
+                    self.stem_dic[stem] = postags
+
+    @staticmethod
+    def _parse_flag(flag):
+        res = []
+        for i in range(12, -1, -1):
+            if flag >= math.pow(2, i):
+                res.append(TurkishStemSuffixCandidateGenerator.TAG_FLAG_MAP[i])
+                flag = flag - math.pow(2, i)
+        if flag != 0:
+            raise IOError("Error: problems in stem flags!")
+        return res
+
+    @staticmethod
+    def _transform_soft_consonants(text):
+        text = re.sub(r"^(.*)b$", r"\1p", text)
+        text = re.sub(r"^(.*)B$", r"\1P", text)
+        text = re.sub(r"^(.*)c$", r"\1ç", text)
+        text = re.sub(r"^(.*)C$", r"\1Ç", text)
+        text = re.sub(r"^(.*)d$", r"\1t", text)
+        text = re.sub(r"^(.*)D$", r"\1T", text)
+        text = re.sub(r"^(.*)ğ$", r"\1k", text)
+        text = re.sub(r"^(.*)Ğ$", r"\1K", text)
+        text = re.sub(r"^(.*)g$", r"\1k", text)
+        text = re.sub(r"^(.*)G$", r"\1K", text)
+        return text
+
+    @staticmethod
+    def _root_transform(candidate_roots):
+        for i in range(len(candidate_roots)):
+            if candidate_roots[i] in TurkishStemSuffixCandidateGenerator.ROOT_TRANSFORMATION_MAP:
+                candidate_roots[i] = TurkishStemSuffixCandidateGenerator.ROOT_TRANSFORMATION_MAP[candidate_roots[i]]
+
+    @staticmethod
+    def _suffix_transform(candidate_suffixes):
+        for i in range(len(candidate_suffixes)):
+            candidate_suffixes[i] = to_lower(candidate_suffixes[i])
+            candidate_suffixes[i] = TurkishStemSuffixCandidateGenerator.\
+                SUFFIX_TRANSFORMATION_REGEX1.sub("A", candidate_suffixes[i])
+            candidate_suffixes[i] = TurkishStemSuffixCandidateGenerator. \
+                SUFFIX_TRANSFORMATION_REGEX2.sub("H", candidate_suffixes[i])
+
+    @staticmethod
+    def _add_candidate_stem_suffix(stem_candidate, suffix_candidate, candidate_roots, candidate_suffixes):
+        # Bana, Sana -> ben, sen
+        if stem_candidate == "ban" and suffix_candidate == "a":
+            candidate_roots.append("ben")
+            candidate_suffixes.append("a")
+        elif stem_candidate == "Ban" and suffix_candidate == "a":
+            candidate_roots.append("Ben")
+            candidate_suffixes.append("a")
+        elif stem_candidate == "san" and suffix_candidate == "a":
+            candidate_roots.append("sen")
+            candidate_suffixes.append("a")
+        elif stem_candidate == "San" and suffix_candidate == "a":
+            candidate_roots.append("Sen")
+            candidate_suffixes.append("a")
+        else:
+            candidate_roots.append(stem_candidate)
+            candidate_suffixes.append(suffix_candidate)
+            if len(stem_candidate) > 2 and len(suffix_candidate) > 0 \
+                    and stem_candidate[-1] == suffix_candidate[0] \
+                    and stem_candidate[-1] in TurkishStemSuffixCandidateGenerator.CONSONANT_STR:
+                # CONSONANT DERIVATION
+                # his -i > hissi, hak -ı > hakkı, red -i > reddi
+                candidate_roots.append(stem_candidate)
+                candidate_suffixes.append(suffix_candidate[1:])
+            elif len(stem_candidate) > 1 and \
+                    TurkishStemSuffixCandidateGenerator.ENDS_NARROW_REGEX.match(stem_candidate) and \
+                    "yor" in suffix_candidate:
+                # bekle -yor > bekliyor, atla -yor > atliyor
+                if stem_candidate.endswith("i") or stem_candidate.endswith("ü"):
+                    candidate_roots.append(stem_candidate[:-1] + "e")
+                    candidate_suffixes.append(suffix_candidate)
+                elif stem_candidate.endswith("ı") or stem_candidate.endswith("u"):
+                    candidate_roots.append(stem_candidate[:-1] + "a")
+                    candidate_suffixes.append(suffix_candidate)
+            if len(stem_candidate) > 2 and \
+                    TurkishStemSuffixCandidateGenerator.ENDS_TWO_CONSONANT_REG.match(stem_candidate) and \
+                    TurkishStemSuffixCandidateGenerator.STARTS_VOWEL_REGEX.match(suffix_candidate):
+                # VOWEL DROP
+                # ağız – ım > ağzım, alın –ın – a > alnına
+                # burun –u > burnu, bağır –ım > bağrım, beyin –i > beyni
+                suffix_start_letter = to_lower(suffix_candidate[0])
+                if suffix_start_letter in ["u", "ü", "ı", "i"]:
+                    candidate_roots.append(stem_candidate[:-1] + suffix_start_letter + stem_candidate[-1])
+                    candidate_suffixes.append(suffix_candidate)
+                elif suffix_start_letter == "e":
+                    candidate_roots.append(stem_candidate[:-1] + "i" + stem_candidate[-1])
+                    candidate_suffixes.append(suffix_candidate)
+                    candidate_roots.append(stem_candidate[:-1] + "ü" + stem_candidate[-1])
+                    candidate_suffixes.append(suffix_candidate)
+                elif suffix_start_letter == "a":
+                    candidate_roots.append(stem_candidate[:-1] + "ı" + stem_candidate[-1])
+                    candidate_suffixes.append(suffix_candidate)
+                    candidate_roots.append(stem_candidate[:-1] + "u" + stem_candidate[-1])
+                    candidate_suffixes.append(suffix_candidate)
+            if TurkishStemSuffixCandidateGenerator.ENDS_WITH_SOFT_CONSONANTS_REGEX.match(stem_candidate):
+                # Softening of consonants
+                candidate_roots.append(TurkishStemSuffixCandidateGenerator._transform_soft_consonants(stem_candidate))
+                candidate_suffixes.append(suffix_candidate)
+
+    def get_stem_suffix_candidates(self, surface_word):
+        candidate_roots = []
+        candidate_suffixes = []
+        for i in range(1, len(surface_word)):
+            candidate_root = surface_word[:i]
+            candidate_suffix = surface_word[i:]
+            if not self.case_sensitive:
+                candidate_root = to_lower(candidate_root)
+                candidate_suffix = to_lower(candidate_suffix)
+            self._add_candidate_stem_suffix(candidate_root, candidate_suffix, candidate_roots, candidate_suffixes)
+        if not self.case_sensitive:
+            candidate_roots.append(to_lower(surface_word))
+        else:
+            candidate_roots.append(to_lower(surface_word))
+        candidate_suffixes.append("")
+        assert len(candidate_roots) == len(candidate_suffixes)
+        TurkishStemSuffixCandidateGenerator._root_transform(candidate_roots)
+        if self.asciification:
+            candidate_roots = [asciify(candidate_root) for candidate_root in candidate_roots]
+            candidate_suffixes = [asciify(candidate_suffix) for candidate_suffix in candidate_suffixes]
+        if self.suffix_normalization:
+            TurkishStemSuffixCandidateGenerator._suffix_transform(candidate_suffixes)
+        return candidate_roots, candidate_suffixes
+
+    def _get_tags(self, suffix, stem_tags=None):
+        if suffix and len(suffix) > 0:
+            if suffix in self.suffix_dic:
+                tags = self.suffix_dic[suffix]
+                if suffix.startswith("'"):
+                    tags += self.suffix_dic[suffix[1:]]
+            elif suffix.startswith("'") and suffix[1:] in self.suffix_dic:
+                tags = self.suffix_dic[suffix[1:]]
+            else:
+                return []
+        else:
+            tags = self.suffix_dic["null"]
+        res = []
+        tags = list(set(tags))
+        for tag in tags:
+            tag_sequences = TurkishStemSuffixCandidateGenerator.TAG_SEPARATOR_REGEX.split(tag)
+            if stem_tags:
+                if tag_sequences[0] in stem_tags or "+".join(tag_sequences[0:2]) in stem_tags:
+                    res.append(tag_sequences)
+            else:
+                res.append(tag_sequences)
+        return res
+
+    def get_analysis_candidates(self, surface_word):
+        candidate_analyzes = []
+        candidate_roots, candidate_suffixes = self.get_stem_suffix_candidates(surface_word)
+        for candidate_root, candidate_suffix in zip(candidate_roots, candidate_suffixes):
+            if TurkishStemSuffixCandidateGenerator.NON_WORD_REGEX.match(candidate_root):
+                stem_tags = ["Punc", "Num", "Noun+Time"]
+            elif len(candidate_suffix) == 0 and candidate_root not in self.stem_dic:
+                stem_tags = ["Noun", "Noun+Prop"]
+            elif candidate_root not in self.stem_dic:
+                if candidate_suffix.startswith("'"):
+                    stem_tags = ["Noun+Prop", "Num"]
+                else:
+                    continue
+            else:
+                stem_tags = self.stem_dic[candidate_root]
+            cur_candidate_analyzes = self._get_tags(candidate_suffix, stem_tags)
+            if cur_candidate_analyzes and len(cur_candidate_analyzes) > 0:
+                candidate_analyzes += [(candidate_root, candidate_suffix, cur_candidate_analysis)
+                                       for cur_candidate_analysis in cur_candidate_analyzes]
+        return candidate_analyzes
+
+
+if __name__ == "__main__":
+    candidate_generator = TurkishStemSuffixCandidateGenerator(case_sensitive=False)
+    print(candidate_generator.get_analysis_candidates("20:00'de"))
+
---
+++ src/models.py
@@ -0,0 +1,287 @@
+# coding=utf-8
+import random
+import math
+from collections import defaultdict
+import dynet as dy
+import numpy as np
+import pickle
+from datetime import datetime
+from data_utils import data_generator
+import logging.config
+
+logging.config.fileConfig('resources/logging.ini')
+logger = logging.getLogger(__file__)
+
+
+class AnalysisScorerModel(object):
+    SENTENCE_BEGIN_TAG = "<s>"
+    SENTENCE_END_TAG = "</s>"
+
+    @classmethod
+    def _create_vocab_chars(cls, sentences):
+        char2id = defaultdict(int)
+        char2id["<"] = len(char2id) + 1
+        char2id["/"] = len(char2id) + 1
+        char2id[">"] = len(char2id) + 1
+        for sentence in sentences:
+            for word in sentence:
+                for ch in word.surface_word:
+                    if ch not in char2id:
+                        char2id[ch] = len(char2id) + 1
+                for root in word.roots:
+                    for ch in root:
+                        if ch not in char2id:
+                            char2id[ch] = len(char2id) + 1
+        return char2id
+
+    @classmethod
+    def _create_vocab_tags(cls, sentences):
+        tag2id = defaultdict(int)
+        for sentence in sentences:
+            for word in sentence:
+                for tags in word.tags:
+                    for tag in tags:
+                        if tag not in tag2id:
+                            tag2id[tag] += len(tag2id) + 1
+        return tag2id
+
+    @classmethod
+    def _encode(cls, tokens, vocab):
+        return [vocab[token] for token in tokens]
+
+    @classmethod
+    def _embed(cls, token, char_embedding_table):
+        return [char_embedding_table[ch] for ch in token]
+
+    def __init__(self, train_from_scratch=True, char_representation_len=100,
+                 word_lstm_rep_len=200, train_data_path="data/data.train.txt",
+                 dev_data_path="data/data.dev.txt", test_data_paths=["data/data.test.txt"],
+                 model_file_name=None, case_sensitive=False):
+        assert word_lstm_rep_len % 2 == 0
+        self.case_sensitive = case_sensitive
+        if train_from_scratch:
+            assert train_data_path
+            assert len(test_data_paths) > 0
+            logger.info("Loading data...")
+            self.train_data_path = train_data_path
+            self.dev_data_path = dev_data_path
+            self.test_data_paths = test_data_paths
+            self.train = data_generator(train_data_path, add_gold_labels=True)
+            self.char2id = self._create_vocab_chars(self.train)
+            self.tag2id = self._create_vocab_tags(self.train)
+            if dev_data_path:
+                self.dev = data_generator(dev_data_path, add_gold_labels=True)
+            else:
+                self.dev = None
+            self.tests = []
+            for test_path in self.test_data_paths:
+                self.tests.append(data_generator(test_path, add_gold_labels=True))
+            logger.info("Creating or Loading Vocabulary...")
+
+            if not self.dev:
+                train_size = int(math.floor(0.99 * len(self.train)))
+                self.dev = self.train[train_size:]
+                self.train = self.train[:train_size]
+            logger.info("Building model...")
+            self.model = dy.Model()
+            self.trainer = dy.AdamTrainer(self.model)
+            self.SURFACE_CHARS_LOOKUP = self.model.add_lookup_parameters(
+                (len(self.char2id) + 2, char_representation_len))
+            self.ROOT_CHARS_LOOKUP = self.model.add_lookup_parameters(
+                (len(self.char2id) + 2, char_representation_len))
+            self.TAGS_LOOKUP = self.model.add_lookup_parameters((len(self.tag2id) + 2, char_representation_len))
+            self.fwdRNN_surface = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.bwdRNN_surface = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.fwdRNN_root = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.bwdRNN_root = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            # self.fwdRNN_suffix = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            # self.bwdRNN_suffix = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.fwdRNN_tag = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.bwdRNN_tag = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+            self.fwdRNN_context = dy.LSTMBuilder(1, word_lstm_rep_len, word_lstm_rep_len, self.model)
+            self.bwdRNN_context = dy.LSTMBuilder(1, word_lstm_rep_len, word_lstm_rep_len, self.model)
+            self.train_model(model_name=model_file_name)
+        else:
+            logger.info("Loading Pre-Trained Model")
+            assert model_file_name
+            self.load_model(model_file_name, char_representation_len, word_lstm_rep_len)
+
+    def propogate(self, sentence):
+        dy.renew_cg()
+        fwd_rnn_surface_init = self.fwdRNN_surface.initial_state()
+        bwd_rnn_surface_init = self.bwdRNN_surface.initial_state()
+        fwd_rnn_root_init = self.fwdRNN_root.initial_state()
+        bwd_rnn_root_init = self.bwdRNN_root.initial_state()
+        # fwd_rnn_suffix_init = self.fwdRNN_suffix.initial_state()
+        # bwd_rnn_suffix_init = self.bwdRNN_suffix.initial_state()
+        fwd_rnn_tag_init = self.fwdRNN_tag.initial_state()
+        bwd_rnn_tag_init = self.bwdRNN_tag.initial_state()
+        fwd_rnn_context_init = self.fwdRNN_context.initial_state()
+        bwd_rnn_context_init = self.bwdRNN_context.initial_state()
+
+        # CONTEXT REPRESENTATIONS
+        surface_words_rep = []
+        for index, word in enumerate(sentence):
+            encoded_surface_word = self._encode(word.surface_word, self.char2id)
+            surface_word_char_embeddings = self._embed(encoded_surface_word, self.SURFACE_CHARS_LOOKUP)
+            fw_exps_surface_word = fwd_rnn_surface_init.transduce(surface_word_char_embeddings)
+            bw_exps_surface_word = bwd_rnn_surface_init.transduce(reversed(surface_word_char_embeddings))
+            surface_word_rep = dy.concatenate([fw_exps_surface_word[-1], bw_exps_surface_word[-1]])
+            surface_words_rep.append(surface_word_rep)
+        fw_exps_context = fwd_rnn_context_init.transduce(surface_words_rep)
+        bw_exps_context = bwd_rnn_context_init.transduce(reversed(surface_words_rep))
+        scores = []
+        # Stem and POS REPRESENTATIONS
+        for index, word in enumerate(sentence):
+            encoded_roots = [self._encode(root, self.char2id) for root in word.roots]
+            # encoded_suffixes = [self._encode(suffix, self.char2id) for suffix in word.suffixes]
+            encoded_tags = [self._encode(tag, self.tag2id) for tag in word.tags]
+            roots_embeddings = [self._embed(root, self.ROOT_CHARS_LOOKUP) for root in encoded_roots]
+            # suffix_embeddings = [self._embed(suffix, self.ROOT_CHARS_LOOKUP) for suffix in encoded_suffixes]
+            tags_embeddings = [self._embed(tag, self.TAGS_LOOKUP) for tag in encoded_tags]
+            analysis_representations = []
+            for root_embedding, tag_embedding in zip(roots_embeddings, tags_embeddings):
+                fw_exps_root = fwd_rnn_root_init.transduce(root_embedding)
+                bw_exps_root = bwd_rnn_root_init.transduce(reversed(root_embedding))
+                root_representation = dy.rectify(dy.concatenate([fw_exps_root[-1], bw_exps_root[-1]]))
+                fw_exps_tag = fwd_rnn_tag_init.transduce(tag_embedding)
+                bw_exps_tag = bwd_rnn_tag_init.transduce(reversed(tag_embedding))
+                tag_representation = dy.rectify(dy.concatenate([fw_exps_tag[-1], bw_exps_tag[-1]]))
+                analysis_representations.append(dy.rectify(dy.esum([root_representation, tag_representation])))
+
+            left_context_rep = fw_exps_context[index]
+            right_context_rep = bw_exps_context[len(sentence) - index - 1]
+            context_rep = dy.tanh(dy.esum([left_context_rep, right_context_rep]))
+            scores.append(
+                (dy.reshape(context_rep, (1, context_rep.dim()[0][0])) * dy.concatenate(analysis_representations, 1))[
+                    0])
+
+        return scores
+
+    def get_loss(self, sentence):
+        scores = self.propogate(sentence)
+        errs = []
+        for i, score in enumerate(scores):
+            root_err = dy.pickneglogsoftmax(score, 0)
+            errs.append(root_err)
+
+        return dy.esum(errs)
+
+    def predict_indices(self, sentence):
+        selected_indices = []
+        scores = self.propogate(sentence)
+        for score in scores:
+            probs = dy.softmax(score)
+            selected_indices.append(np.argmax(probs.npvalue()))
+        return selected_indices
+
+    def predict_probs(self, sentence):
+        res = []
+        scores = self.propogate(sentence)
+        for score in scores:
+            probs = dy.softmax(score)
+            res.append(probs.npvalue())
+        return res
+
+    def calculate_acc(self, sentences):
+        corrects = 0
+        non_ambigious_count = 0
+        total = 0
+        for sentence in sentences:
+            predicted_label_indexes = self.predict_indices(sentence)
+            predicted_labels = [word.roots[i] + "+".join(word.tags[i])
+                                for i, word in zip(predicted_label_indexes, sentence)]
+            gold_labels = [word.roots[0] + "+".join(word.tags[0]) for i, word in enumerate(sentence)]
+            corrects += [1 for l1, l2 in zip(gold_labels, predicted_labels) if l1 == l2].count(1)
+            non_ambigious_count += [1 for w in sentence if len(w.roots) == 1].count(1)
+            total += len(sentence)
+        return (corrects * 1.0 / total), ((corrects - non_ambigious_count) * 1.0 / (total - non_ambigious_count))
+
+    def train_model(self, model_name="model", early_stop=False, num_epoch=20):
+        logger.info("Starting training...")
+        max_acc = 0.0
+        epoch_loss = 0
+        for epoch in range(num_epoch):
+            logger.info("Loading data...")
+            self.train = data_generator(self.train_data_path, add_gold_labels=True)
+            if self.dev_data_path:
+                self.dev = data_generator(self.dev_data_path, add_gold_labels=True)
+            self.tests = []
+            for test_path in self.test_data_paths:
+                self.tests.append(data_generator(test_path, add_gold_labels=True))
+            t1 = datetime.now()
+            count = 0
+            for i, sentence in enumerate(self.train, 1):
+                loss_exp = self.get_loss(sentence)
+                cur_loss = loss_exp.scalar_value()
+                epoch_loss += cur_loss
+                loss_exp.backward()
+                self.trainer.update()
+                if i % 100 == 0:  # logger.info status
+                    t2 = datetime.now()
+                    delta = t2 - t1
+                    logger.info("\rloss = {}  /  {} instances finished in  {} seconds     "
+                                .format(epoch_loss / (i * 1.0), i, delta.seconds))
+                count = i
+            t2 = datetime.now()
+            delta = t2 - t1
+            logger.info("epoch {} finished in {} minutes. loss = {}"
+                        .format(epoch, delta.seconds / 60.0, epoch_loss / count * 1.0))
+            logger.info("Calculating Accuracy on dev set")
+            epoch_loss = 0
+            acc, amb_acc = self.calculate_acc(self.dev)
+            logger.info("Accuracy on dev set:{}\nAmbiguous accuracy on dev set:{} ".format(acc, amb_acc))
+            if acc > max_acc:
+                max_acc = acc
+                logger.info("Max accuracy increased, saving model...")
+                self.save_model(model_name)
+            elif early_stop and max_acc > acc:
+                logger.info("Max accuracy did not increase, early stopping!")
+                break
+
+            logger.info("Calculating Accuracy on test sets")
+            for q in range(len(self.test_data_paths)):
+                logger.info("Calculating Accuracy on test set: {}".format(self.test_data_paths[q]))
+                acc, amb_acc = self.calculate_acc(self.tests[q])
+                logger.info(" accuracy: {}    ambiguous accuracy: {}".format(acc, amb_acc))
+
+    def save_model(self, model_name):
+        self.model.save("resources/models/" + model_name + ".model")
+        with open("resources/models/" + model_name + ".char2id", "wb") as f:
+            pickle.dump(self.char2id, f)
+        with open("resources/models/" + model_name + ".tag2id", "wb") as f:
+            pickle.dump(self.tag2id, f)
+
+    def load_model(self, model_name, char_representation_len, word_lstm_rep_len):
+        with open("resources/models/" + model_name + ".char2id", "rb") as f:
+            self.char2id = pickle.load(f)
+        with open("resources/models/" + model_name + ".tag2id", "rb") as f:
+            self.tag2id = pickle.load(f)
+
+        self.model = dy.Model()
+        self.trainer = dy.AdamTrainer(self.model)
+        self.CHARS_LOOKUP = self.model.add_lookup_parameters(
+            (len(self.char2id) + 2, char_representation_len))
+        self.TAGS_LOOKUP = self.model.add_lookup_parameters((len(self.tag2id) + 2, char_representation_len))
+        self.fwdRNN_surface = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.bwdRNN_surface = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.fwdRNN_root = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.bwdRNN_root = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        # self.fwdRNN_suffix = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        # self.bwdRNN_suffix = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.fwdRNN_tag = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.bwdRNN_tag = dy.LSTMBuilder(1, char_representation_len, word_lstm_rep_len / 2, self.model)
+        self.fwdRNN_context = dy.LSTMBuilder(1, word_lstm_rep_len, word_lstm_rep_len, self.model)
+        self.bwdRNN_context = dy.LSTMBuilder(1, word_lstm_rep_len, word_lstm_rep_len, self.model)
+        self.model.load("resources/models/" + model_name + ".model")
+
+    @staticmethod
+    def create_from_existed_model(model_name):
+        return AnalysisScorerModel(train_from_scratch=False, model_file_name=model_name)
+
+
+if __name__ == "__main__":
+    AnalysisScorerModel(train_data_path="data/data.train.txt", dev_data_path="data/data.dev.txt",
+                        test_data_paths=["data/test.merge", "data/data.test.txt",
+                                         "data/Morph.Dis.Test.Hand.Labeled-20K.txt"],
+                        model_file_name="lookup_disambiguator_wo_suffix", train_from_scratch=True)
